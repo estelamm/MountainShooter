b9f53209e85ccac46c75dc0f6aee7a1b26eec55b
Estela Matarredona <estelammatarredona@gmail.com>
2025-03-05 16:50:40 -0300

    changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -20,8 +20,8 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)  # desenhar imagem no retangulo
-            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", COLOR_WHITE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_WHITE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
