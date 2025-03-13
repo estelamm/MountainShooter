5c88bb2a33593600121a4e86175c495b77756c27
Estela Matarredona <estelammatarredona@gmail.com>
2025-03-13 16:49:14 -0300

    Creating Meadiator Pattern. Enemies Destruction.

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,11 @@
-#C
+# C
 import pygame
 
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 0)
 
-#E
+# E
 EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -17,18 +17,32 @@ ENTITY_SPEED = {
     'Level1Bg6': 6,
     'Player1': 3,
     'Player2': 3,
-    'Enemy1' : 2,
-    'Enemy2' : 1,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
-#M
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+}
+
+# M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
 
-#P
+# P
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
@@ -40,9 +54,9 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
-#S
+# S
 SPAWN_TIME = 4000
 
-#W
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/Level.py
+++ code/Level.py
@@ -11,6 +11,7 @@ from pygame.font import Font
 from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -20,13 +21,11 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg')) #Background
-        self.entity_list.append(EntityFactory.get_entity('Player1')) #Player1
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))  # Background
+        self.entity_list.append(EntityFactory.get_entity('Player1'))  # Player1
         if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
             self.entity_list.append(EntityFactory.get_entity('Player2'))
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
-
-
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
@@ -50,6 +49,9 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps() : .0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # Collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
