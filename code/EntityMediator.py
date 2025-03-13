0cc77383a67d0fa50c07cfaad02234e2726604e7
Estela Matarredona <estelammatarredona@gmail.com>
2025-03-13 18:07:33 -0300

    Creating Player Shot. Creating Enemy Shot.

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,21 +1,29 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
 
     @staticmethod
-    def __verify_collision_window(ent: Entity): #metodo privado que só funciona dentro da class
+    def __verify_collision_window(ent: Entity):  # metodo privado que só funciona dentro da class
         if isinstance(ent, Enemy):
             if ent.rect.right < 0:
                 ent.health = 0
-
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
+                ent.health = 0
 
     @staticmethod
-    def verify_collision(entity_list: list[Entity]): #criando entidades
-        for i in range(len(entity_list)): #iterando entidades
+    def verify_collision(entity_list: list[Entity]):  # criando entidades
+        for i in range(len(entity_list)):  # iterando entidades
             test_entity = entity_list[i]
-            EntityMediator.__verify_collision_window(test_entity) #novo metodo
+            EntityMediator.__verify_collision_window(test_entity)  # novo metodo
 
     @staticmethod
     def verify_health(entity_list: list[Entity]):
